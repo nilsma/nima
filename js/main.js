480878587e36e244332b9bb59ee30aeb89c03747
root <root@nma-acer.(none)>
2014-02-09 12:48:38 +0100

    added new project(JAT)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/**
+* A function to remove the displayed links of each projects link
+* to the given projects showcase-page
+*/
 function removeLinks(nodelist) {
     var elements = nodelist;
     for(var i = 0; i < elements.length; ++i) {
@@ -6,6 +10,12 @@ function removeLinks(nodelist) {
     }
 }
 
+/**
+* A function to find the given projects link to that projects
+* showcase-page on click anywhere on that given projects encompassing
+* .project-element. Also opens that link in a new tab
+* @param element HTMLElement - the section.project HTML element
+*/
 function openProject(element) {
     var nodes = element.childNodes;
     for(var i = 0; i < nodes.length; i++) {
@@ -24,6 +34,30 @@ function openProject(element) {
     }
 }
 
+/**
+* A function to set the about.html profile image to colored on mouseenter
+* and back to gray-scale on mouseleave
+*/
+function colorMe() {
+    var path;
+    $('section#about-me img').mouseenter(function() {
+	path = $(this).attr('src');
+	path = path.substring(0, path.length - 9);
+	path = path.concat('.jpg');
+	document.getElementsByTagName('img')[0].src = path;
+    }).mouseleave(function() {
+	path = $(this).attr('src');
+	path = path.substring(0, path.length - 4);
+	path = path.concat('_gray.jpg');
+	document.getElementsByTagName('img')[0].src = path;
+    });
+}
+
+/**
+* A function to set all other projects' thumbnails to gray-scale pictures
+* on mouseenter on a section.project element. Also sets all other projects'
+* thumbnails back to colored pictures on mouseleave
+*/
 function hideProjects() {
     jQuery('.project').mouseenter(function() {
 	var projects = document.getElementsByClassName('project');
@@ -42,29 +76,57 @@ function hideProjects() {
     });
 }
 
+/**
+* A function to get the relative path to a projects thumbnail within
+* that projects image element
+* @param project HTMLElement - the .project HTML element of which to get the image path
+* @return path string - the relative path of the image
+*/
 function getPath(project) {
     var path = project.getElementsByTagName('img')[0].getAttribute('src');
     return path;
 }
 
-function buildGrayPath(string) {
-    var stripped = string.substring(0, string.length - 4);
+/**
+* A function to build the new relative path to the given thumbnails
+* gray-scale equivalent thumbnail
+* @param path string - the relative path to the currently displayed colored thumbnail
+* @return appended string - the relative path to the gray-scale equivalent
+*/ 
+function buildGrayPath(path) {
+    var stripped = path.substring(0, path.length - 4);
     var appended = stripped.concat('_gray.png');
     return appended;
 }
 
+/**
+* A function to build the new relative path to the given thumbnails
+* colored equivalent thumbnail
+* @param path string - the relative path to the currently displayed gray-scale thumbnail
+* @return appended string - the relative path to the colored thumbnail equivalent
+*/
 function buildColorsPath(string) {
     var stripped = string.substring(0, string.length - 9);
     var appended = stripped.concat('.png');
     return appended;
 }
 
+/**
+* A function to set the project's image src attribute to point to
+* the project's colored thumbnail
+* @param project HTMLElement - the HTML element of the given section.project
+*/
 function setColors(project) {
     var path = getPath(project);
     var colorsPath = buildColorsPath(path);
     project.getElementsByTagName('img')[0].src = colorsPath;
 }
 
+/**
+* A function to set the project's image src attribute to point to
+* the project's gray-scale thumbnail
+* @param project HTMLElement - the HTML element of the given section.project
+*/
 function setGray(project) {
     var path = getPath(project);
     var grayPath = buildGrayPath(path);
@@ -74,5 +136,6 @@ function setGray(project) {
 window.onload = function onLoad() {
     var elements = document.getElementsByClassName('project-url');
     removeLinks(elements);
-    hideProjects();
+//    hideProjects();
+//    colorMe();
 }
